493787c77e0011e35148cf8fa14a122710431421
Hồ CNTT-1801 Ngải Dùng <ngaiho970@gmail.com>
2026-07-06 09:27:46 +0700

    config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+class Config:
+    SECRET_KEY = "movie_booking_secret"
+
+    MYSQL_HOST = "localhost"
+    MYSQL_USER = "root"
+    MYSQL_PASSWORD = "ngaiquyhung"
+    MYSQL_DATABASE = "movie_booking"
+    MYSQL_PORT = 3306
